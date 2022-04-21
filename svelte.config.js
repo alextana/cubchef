1a35dc62405697b3c71b06232c78c1fa36756bf1
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-04-22 00:21:47 +0100

    change adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +12,6 @@ const config = {
 			postcss: true
 		})
 	]
-};
+}
 
-export default config;
+export default config
