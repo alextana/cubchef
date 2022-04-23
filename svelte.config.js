a0b02c84201d8a1f586cc03fe2daede4d95e0b43
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-04-23 14:34:21 +0100

    change port

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import node from '@sveltejs/adapter-node'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node({ env: { port: process.env.PORT } }),
+		adapter: node({ env: { port: 3000 } }),
 	},
 
 	preprocess: [
